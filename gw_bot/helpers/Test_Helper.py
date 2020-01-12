50106330457616a96ce5a0b952f01f3597d8a4e1
Dinis Cruz <dinis.cruz@owasp.org>
2020-01-12 06:39:26 +0000

    Added couple methods to Test_Helper

--- gw_bot/helpers/Test_Helper.py
+++ gw_bot/helpers/Test_Helper.py
@@ -1,3 +1,4 @@
+import base64
 from unittest import TestCase
 
 from pbx_gs_python_utils.utils.Dev import Dev
@@ -19,3 +20,14 @@ class Test_Helper(TestCase):
     def lambda_package(self, lambda_name):
         self.result = None
         return OSS_Setup().setup_test_environment().lambda_package(lambda_name)
+
+    @staticmethod
+    def print(result):
+        if result is not None:
+            Dev.pprint(result)
+
+    @staticmethod
+    def save_png(png_data, target_file):
+        if png_data is not None:
+            with open(target_file, "wb") as fh:
+                fh.write(base64.decodebytes(png_data.encode()))
