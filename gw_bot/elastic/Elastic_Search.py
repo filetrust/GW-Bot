f8afe8f2abc3a4f70577b03129587ff629bb8c61
Dinis Cruz <dinis.cruz@owasp.org>
2020-02-02 15:04:34 +0000

    minor fix and updated Xref

--- gw_bot/elastic/Elastic_Search.py
+++ gw_bot/elastic/Elastic_Search.py
@@ -56,14 +56,17 @@ class Elastic_Search:
         return self.es.index(index=self.index, doc_type='item', body=data)
 
     def add(self,data, id_key = None):
+        if data is None or data == {}:
+            return {'error': 'no data provided to send to ELK'}
         try:
             if id_key is not None:
                 return self.es.index(index=self.index, doc_type='item', body=data, id=data[id_key])
             else:
                 return self.es.index(index=self.index, doc_type='item', body=data)
         except Exception as error:
-            print(error)
-            return {"elk-error": "{0}".format(error)}
+            message = f'in Elastic_Search:add_data_with_timestamp: {error}'
+            print(message)
+            return {"elk-error": "{0}".format(message)}
 
     def add_bulk(self, data, id_key = None, pipeline = None):
         ok = 0
