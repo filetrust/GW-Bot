f3bccb0e5234afdca4308be4a66c1f33bae82b8e
Dinis Cruz <dinis.cruz@owasp.org>
2020-02-16 13:31:35 +0000

    First working version of SaaS VPS

--- gw_bot/lambdas/gw/proxy/saas_vps.py
+++ gw_bot/lambdas/gw/proxy/saas_vps.py
@@ -1,10 +1,61 @@
-def run(event, context):
+import base64
 
-    message =  'store saas will go here'
+from gw_bot.helpers.Lambda_Helpers import log_to_elk
+from gw_bot.lambdas.png_to_slack import load_dependency
 
+def log_request(path, method, headers):
+    data = { 'path': path,'method': method, 'headers':headers }
+    log_to_elk('proxy message', data)
+
+def run(event, context=None):
+    load_dependency('requests')
+    import requests
+    path            = event.get('path','')
+    method          = event.get('httpMethod','')
+    headers         = event.get('headers',{})
+    log_request(path, method,headers)
+    request_headers = {'accept'         : headers.get('headers'        ),
+                       'User-Agent'     : headers.get('User-Agent'     ),
+                       'accept-encoding': headers.get('accept-encoding')}
+
+    target = f'https://glasswall-file-drop.azurewebsites.net{path}'
+    response = requests.get(target,headers=request_headers)
+    response_headers = {}
+
+    response_body    = response.content
+
+    for key, value in response.headers.items():           # the original value of result.headers is not serializable
+        response_headers[key] = str(value)
+
+    content_type = response_headers.get('Content-Type')
+
+    #message =  f'store saas will go here!!!: {event}'
+
+    binary_types = [
+        "application/octet-stream",
+        "application/x-protobuf",
+        "application/x-tar",
+        "application/zip",
+        "image/png",
+        "image/jpeg",
+        "image/jpg",
+        "image/tiff",
+        "image/webp",
+        "image/jp2",
+        'font/woff',
+        'font/woff2'
+    ]
+
+
+    if content_type in binary_types:
+        is_base_64=True
+        response_body = base64.b64encode(response_body).decode("utf-8")
+    else:
+        is_base_64 = False
+        response_body = response.text
     return {
-        "isBase64Encoded": False,
-        "statusCode": 200,
-        "headers": {},
-        "body": f'{message}'
+        "isBase64Encoded": is_base_64,
+        "statusCode"     : 200,
+        "headers"        : response_headers,
+        "body"           : response_body
     }
--- tests/unit/lambdas/gw/proxy/test_saas_vps.py
+++ tests/unit/lambdas/gw/proxy/test_saas_vps.py
@@ -15,12 +15,30 @@ class test_saas_vps(Test_Helper):
         self.aws_lambda.update_code()
 
     def test__invoke_directy(self):
-        payload = {}
+        #payload = {'path':'/favicon-shard.png'}
+        payload = {'path': '/'}
         self.result = run(payload, {})
 
     def test__invoke_via_lambda(self):
+        payload = {'path': '/favicon-shard.png'}
+        payload = {'path': ''}
         self.test_update_lambda()
-        self.result = self.aws_lambda.invoke()
+        self.result = self.aws_lambda.invoke(payload)
+        #self.png_data = self.result.get('body')
+
+    def test_invoke_directly_with_payload(self):
+        payload = {'resource': '/', 'path': '/', 'httpMethod': 'GET', 'headers': {'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'accept-encoding': 'gzip, deflate, br', 'accept-language': 'en-GB,en;q=0.9,pt-PT;q=0.8,pt;q=0.7,en-US;q=0.6', 'Host': 'gw-proxy.com', 'sec-fetch-mode': 'navigate', 'sec-fetch-site': 'none', 'sec-fetch-user': '?1', 'upgrade-insecure-requests': '1', 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36', 'X-Amzn-Trace-Id': 'Root=1-5e48be59-9bbf9c36970c2c503b01ae20', 'X-Forwarded-For': '82.39.36.190', 'X-Forwarded-Port': '443', 'X-Forwarded-Proto': 'https'}, 'multiValueHeaders': {'accept': ['text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'], 'accept-encoding': ['gzip, deflate, br'], 'accept-language': ['en-GB,en;q=0.9,pt-PT;q=0.8,pt;q=0.7,en-US;q=0.6'], 'Host': ['gw-proxy.com'], 'sec-fetch-mode': ['navigate'], 'sec-fetch-site': ['none'], 'sec-fetch-user': ['?1'], 'upgrade-insecure-requests': ['1'], 'User-Agent': ['Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'], 'X-Amzn-Trace-Id': ['Root=1-5e48be59-9bbf9c36970c2c503b01ae20'], 'X-Forwarded-For': ['82.39.36.190'], 'X-Forwarded-Port': ['443'], 'X-Forwarded-Proto': ['https']}, 'queryStringParameters': None, 'multiValueQueryStringParameters': None, 'pathParameters': None, 'stageVariables': None, 'requestContext': {'resourceId': 'g72apoktf2', 'resourcePath': '/', 'httpMethod': 'GET', 'extendedRequestId': 'H-KuBFP3joEFiuQ=', 'requestTime': '16/Feb/2020:04:00:25 +0000', 'path': '/', 'accountId': '311800962295', 'protocol': 'HTTP/1.1', 'stage': 'Prod', 'domainPrefix': 'gw-proxy', 'requestTimeEpoch': 1581825625683, 'requestId': '3ff4b3e5-5503-4750-9ef5-cf61b888d552', 'identity': {'cognitoIdentityPoolId': None, 'accountId': None, 'cognitoIdentityId': None, 'caller': None, 'sourceIp': '82.39.36.190', 'principalOrgId': None, 'accessKey': None, 'cognitoAuthenticationType': None, 'cognitoAuthenticationProvider': None, 'userArn': None, 'userAgent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36', 'user': None}, 'domainName': 'gw-proxy.com', 'apiId': 'l2zujsuve3'}}
+        self.result = run(payload,{})
+
+    def test_invoke_directly__GET_root_path(self):
+        payload = { 'path': '/aaa', 'httpMethod': 'GET', 'headers': {'accept': 'text/html,application/xhtml+xml,application/xml'}}
+        self.result = run(payload)
+
+
+
+
+
+
 
 
 class test_Rest_API__SaaS_VPs(Test_Helper):
@@ -30,12 +48,26 @@ class test_Rest_API__SaaS_VPs(Test_Helper):
         self.api_name    = 'lambda-proxy'
         self.lambda_name = 'gw_bot_lambdas_gw_proxy_saas_vps'
 
-    def test_setup_lambda_route(self):
-
+    def test_setup_lambda_route(self):                      # will create a {proxy+} integration
         rest_api    = Rest_API(self.api_name).create()
-        rest_api.add_method_lambda('/','GET',self.lambda_name)
+        parent_id = rest_api.resource_id('/')
+        rest_api.api_gateway.resource_create(rest_api.id(),parent_id,'{proxy+}')
+        self.result = rest_api.add_method_lambda('/'        , 'ANY', self.lambda_name)  # need to add both
+        self.result = rest_api.add_method_lambda('/{proxy+}', 'ANY', self.lambda_name)  # since this one wasn't catching the root requests
         rest_api.deploy()
-        self.result = rest_api.test_method('/','GET')
+        #self.result = rest_api.test_method('/','GET')
+
+    def test_add_proxy_route(self):
+        rest_api = Rest_API(self.api_name)
+        rest_api_id = rest_api.id()
+        parent_id = rest_api.resource_id('/')
+        #rest_api.api_gateway.resource_create(rest_api_id,parent_id,'{proxy+}')
+        #self.result = rest_api.add_method_lambda('/{proxy+}', 'ANY', self.lambda_name)
+        rest_api.deploy()
+        # self.result = rest_api_id.add_method_lambda('/', '{proxy+}')
+        #self.result = rest_api.api_gateway.resources(rest_api_id)
+
+
 
     def test_GET_request(self):
         self.result = GET(Rest_API(self.api_name).url())
