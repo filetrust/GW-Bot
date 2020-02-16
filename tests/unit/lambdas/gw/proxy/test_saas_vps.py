c6cb289eae440b9edfa701b9f05da71f906271b8
Dinis Cruz <dinis.cruz@owasp.org>
2020-02-16 22:58:42 +0000

    Added methods that helped to create the SSL environment for SaaS-vps SSL setup

--- tests/unit/lambdas/gw/proxy/test_saas_vps.py
+++ tests/unit/lambdas/gw/proxy/test_saas_vps.py
@@ -3,6 +3,7 @@ from pbx_gs_python_utils.utils.Http import GET
 
 from gw_bot.helpers.Test_Helper import Test_Helper
 from gw_bot.lambdas.gw.proxy.saas_vps import run
+from osbot_aws.apis.API_Gateway import API_Gateway
 from osbot_aws.helpers.Rest_API import Rest_API
 
 class test_saas_vps(Test_Helper):
@@ -41,12 +42,14 @@ class test_saas_vps(Test_Helper):
 
 
 
+# this call and methods contain the multiple bits required to set up the proxy (todo: consolidate in one end-to-end solution to be executed on a clear AWS region/organisation)
 class test_Rest_API__SaaS_VPs(Test_Helper):
 
     def setUp(self):
         super().setUp()
         self.api_name    = 'lambda-proxy'
         self.lambda_name = 'gw_bot_lambdas_gw_proxy_saas_vps'
+        self.api_gateway = API_Gateway()
 
     def test_setup_lambda_route(self):                      # will create a {proxy+} integration
         rest_api    = Rest_API(self.api_name).create()
@@ -57,19 +60,13 @@ class test_Rest_API__SaaS_VPs(Test_Helper):
         rest_api.deploy()
         #self.result = rest_api.test_method('/','GET')
 
-    def test_add_proxy_route(self):
-        rest_api = Rest_API(self.api_name)
-        rest_api_id = rest_api.id()
-        parent_id = rest_api.resource_id('/')
-        #rest_api.api_gateway.resource_create(rest_api_id,parent_id,'{proxy+}')
-        #self.result = rest_api.add_method_lambda('/{proxy+}', 'ANY', self.lambda_name)
-        rest_api.deploy()
-        # self.result = rest_api_id.add_method_lambda('/', '{proxy+}')
-        #self.result = rest_api.api_gateway.resources(rest_api_id)
+    def test_deploy_api(self):
+        rest_api = Rest_API(self.api_name).create()
+        self.result = rest_api.deploy()
 
-    # here is the test that added the A record
+    # here is the test that added the A record for both top level domain and child domains
     def test_record_set_upsert(self):
-        from osbot_aws.apis.Route_53 import Route_53
+
         name                = 'gw-proxy.com.'
         record_type         = 'A'
         dns_name            = 'd-noho75bpih.execute-api.eu-west-1.amazonaws.com.'
@@ -77,5 +74,45 @@ class test_Rest_API__SaaS_VPs(Test_Helper):
         alias_hosted_zone_id = 'ZLY8HYME6SFDD'
         self.result = Route_53().record_set_upsert(name, record_type, dns_name,hosted_zone_id,alias_hosted_zone_id)
 
+    def test_record_set_upsert(self):
+        from osbot_aws.apis.Route_53 import Route_53
+        name                = '*.gw-proxy.com.'
+        record_type         = 'A'
+        dns_name            = 'd-noho75bpih.execute-api.eu-west-1.amazonaws.com.'
+        hosted_zone_id      = '/hostedzone/ZMHOWKWA1ZN69'
+        alias_hosted_zone_id = 'ZLY8HYME6SFDD'
+        self.result = Route_53().record_set_upsert(name, record_type, dns_name,hosted_zone_id,alias_hosted_zone_id)
+
+    # here is the test that asked for the wildcart Cert to be created
+    def test_certificate_request(self):
+        #self.result = self.acm.certificate_request('*.gw-proxy.com')
+        self.result = self.acm.certificate('arn:aws:acm:eu-west-1:311800962295:certificate/1f191c3a-0214-4ef5-9f03-27cc0b46bef3')
+
     def test_GET_request(self):
         self.result = GET(Rest_API(self.api_name).url())
+
+    # adding domain to API Gateway
+    # gw-proxy.com
+    def test_domain_name_add_path_mapping(self):
+        rest_api_id = self.api_gateway.rest_api_id('lambda-proxy')
+        domain_name = 'gw-proxy.com'
+        base_path   = ''
+        self.result = self.api_gateway.domain_name_add_path_mapping(rest_api_id=rest_api_id,domain_name=domain_name,base_path=base_path)
+
+    def test_domain_name_create(self):
+        domain_name     = 'gw-proxy.com'
+        certificate_arn = 'arn:aws:acm:eu-west-1:311800962295:certificate/cac6ccbe-e5a6-41b1-ab87-461ff1d88458'
+        self.result     = self.api_gateway.domain_name_create__regional(domain_name=domain_name, certificate_arn=certificate_arn)
+
+    # *.gw-proxy.com
+
+    def test_domain_name_create(self):
+        domain_name     = '*.gw-proxy.com'
+        certificate_arn = 'arn:aws:acm:eu-west-1:311800962295:certificate/be75edef-b698-4425-bf7a-7a129bb0331e'
+        self.result     = self.api_gateway.domain_name_create__regional(domain_name=domain_name, certificate_arn=certificate_arn)
+
+    def test_domain_name_add_path_mapping(self):
+        rest_api_id = self.api_gateway.rest_api_id('lambda-proxy')
+        domain_name = '*.gw-proxy.com'
+        base_path   = ''
+        self.result = self.api_gateway.domain_name_add_path_mapping(rest_api_id=rest_api_id,domain_name=domain_name,base_path=base_path)
